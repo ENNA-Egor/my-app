3ad5f81e26cc9f1bb85315e9f5e4cd3db3926b2f
ENNA-Egor <e.yudakov@gmail.com>
2024-10-17 09:09:05 +0300

    Добавил изменения, но ещё есь ошибки

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import {createStore} from 'redux';
+
+import {rootReduser} from './filters';
+
+const store = createStore (
+    rootReduser,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+export {store};
