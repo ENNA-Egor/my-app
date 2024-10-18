d04c0b3a28abe8f03c0f88690bbb283b3fd68832
ENNA-Egor <e.yudakov@gmail.com>
2024-10-18 13:58:55 +0300

    Добавил фильтры.

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,22 @@
 // import data from '../mock/data.json';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import { JobPosition } from './JobPosition';
 import {selectAllCard} from '../store/cards/cards-selectors'
 
+import {addFilter} from '../store/filters/filters-actions'
+
 const JobList = () => {
+
+  const dispatch = useDispatch();
   const cards = useSelector(selectAllCard);
+
+  const handleAddFilter = (filter)=> {
+    dispatch (addFilter(filter));
+  }
   return (
     <div className='job-list'>
       {cards.map(item => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition key={item.id} {...item} handleAddFilter = {handleAddFilter} />
       ))}
     </div>
   )
--- src/store/root-reduser.js
+++ src/store/root-reduser.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 
-import {cardsReduser} from './cards/cards-reduser'
+import {cardsReduser} from './cards/cards-reduser';
+import {filterReduser} from './filters/filters-reduser'
 
 
 export const rootReduser = combineReducers ({
     cards: cardsReduser, 
+    filters: filterReduser,
 }); 
