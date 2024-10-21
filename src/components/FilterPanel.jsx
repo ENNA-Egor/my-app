a153edffd09d67202a52b4f6c8970b4569096de6
ENNA-Egor <e.yudakov@gmail.com>
2024-10-21 08:45:00 +0300

    Добавил выбор фильтров

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,16 +1,30 @@
+import {useSelector, useDispatch} from 'react-redux';
+
 import { Badge } from 'UI/Badge';
 import { Card } from 'UI/Card';
 import { Stack } from 'UI/Stack';
 
+import {selectFilters} from '../store/filters/filters-selectors'
+
 
 const FilterPanel = () => {
+  const curentFilter = useSelector(selectFilters);
+
+  if (curentFilter.length === 0){
+    return null;
+  }
+
   return (
     <Card className="filter-panel">
       <div className="filter-panel-wrapper">
         <Stack>
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
+          {curentFilter.map(filter=> (
+          <Badge
+           key = {filter}
+           variant="clearable">
+            {filter}
+           </Badge>
+          ))}
         </Stack>
 
         <button className='link'>Clear</button>
